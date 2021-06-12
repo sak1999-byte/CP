46081c03e8efcdb7b9f758c87de8fac00b8f326a
Sahil <67081173+Sahil-stack@users.noreply.github.com>
2021-06-12 09:38:14 +0530

    Add Two Number

--- Java/hello.java
+++ Java/hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello");
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
     }
 }
