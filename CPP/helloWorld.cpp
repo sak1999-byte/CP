1fbfb0f1d3bf19df9077e99c0d50236d310686e7
Sahil <67081173+Sahil-stack@users.noreply.github.com>
2021-06-16 15:57:45 +0530

    CPP

--- CPP/helloWorld.cpp
+++ CPP/helloWorld.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    cout << "Hello Mac";
-    cout << "Done";
-    cout<<"Hi This is Vim"<<endl;
-    int a=10;
-    int b=20;
-    cout<<a+b;
+int main(){
+	cout<<"Hello World"<<endl;
 }
