404dcea40cba40213d7d2256cd4d91e450c83c4b
Sahil <67081173+Sahil-stack@users.noreply.github.com>
2021-06-16 17:57:49 +0530

    Intro To CPP

---
+++ CPP/evenOdd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Enter  number : "<<endl;
+int a;
+cin>>a;
+if(a%2==0){
+cout<<"Number is even"<<endl;
+}else{
+	cout<<"Number is odd"<<endl;
+}
+}
---
+++ CPP/fahToCel.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"Enter Fah value :"<<endl;
+int f;
+cin>>f;
+int c=(5.0/9)*(f-32);
+cout<<10%3<<endl;
+cout<<c<<endl;
+
+int a;
+int b;
+cout<<"Enter the numbers :" <<endl;
+
+cin>>a>>b;
+bool isEqual=(a==b);
+bool isAGreater=(a>b);
+bool isALess=(a<b);
+cout<<"Are they Equal"<<isEqual<<endl;
+cout<<"is A Greater"<<isAGreater<<endl;
+cout<<"is A less"<<isALess<<endl;
+}
